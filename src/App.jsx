e15504bf858fceef83b4ab434eeddcfaaf583547
unknown <dhenizelopez@gmail.com>
2025-05-10 12:53:45 +0800

    UPDATE: Under development, fix some functions

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { useState } from 'react'
 import './App.css'
+import TodoList from './TodoList'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      
-    </>
+    <div>
+      <TodoList />
+    </div>
   )
 }
 
--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const TodoList = () => {
+export default function TodoList() {
+
+  const [count, setCount] = useState(0);
+
+  const handleIncrement = () => {
+    setCount(count + 1);
+  };
+
+  const handleDecrement = () => {
+    setCount(count - 1);
+  };
+
   return (
     <div>
-        
+      <button onClick = {handleIncrement}>Increment</button>
+      <button onClick = {handleDecrement}>Decrement</button>
+      <p>{count}</p>
     </div>
   )
 }
 
-export default TodoList
